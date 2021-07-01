6c07a8eae8c0f9ead0fa1528177b0e889873acc8
zia <zia.csco@gmail.com>
2021-07-01 09:06:32 +0430

    forget password is fixex

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -33,7 +33,7 @@ class UserSeeder extends Seeder
             ],
             [
                 'name' => 'Mover A',
-                'email' => 'tingsapp4@gmail.com',
+                'email' => 'zia.googl@gmail.com',
                 'phone' => '+12504000000',
                 'phone_verified_at' => $today,
                 'password' => Hash::make('123')
